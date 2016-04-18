39ce99b693a3978713a080002da0c84fe4574ec9
Realeyz <mark@themarksmen.net>
2016-04-18 14:39:34 -0400

    rotation slightly improved
    but it needs to occur only within a range of rotation

--- MIDI_Spotlight/Assets/Scripts/MyRotate.cs
+++ MIDI_Spotlight/Assets/Scripts/MyRotate.cs
@@ -3,25 +3,26 @@ using System.Collections;
 using MidiJack;
 
 public class MyRotate : MonoBehaviour {
-
-	public int knobNumber;
+	public int knobNumber = 16;
 	[SerializeField]
 	private float knobVal; // value of knob
 	[SerializeField]
 	private float rotateSpeed = 1.0f; // In rotations per second
 
+
 	void Awake()
 	{
 		knobVal = MidiMaster.GetKnob(knobNumber);
 		// transform.localScale = new Vector3(1, s, 1);
-		transform.Rotate (transform.up, 360 * rotateSpeed * knobVal); //transform.up = around Y axis
+		transform.Rotate (transform.up, rotateSpeed * knobVal); //transform.up = around Y axis
 	}
 
 
 	void Update()
 	{
+		// var channels = MidiMaster.GetKnobNumbers();
 		knobVal = MidiMaster.GetKnob(knobNumber);
 		// transform.localScale = new Vector3(1, s, 1);
-		transform.Rotate (transform.up, 360 * rotateSpeed * knobVal); //transform.up = around Y axis
+		transform.Rotate (transform.up, rotateSpeed * knobVal); //transform.up = around Y axis
 	}
 }
