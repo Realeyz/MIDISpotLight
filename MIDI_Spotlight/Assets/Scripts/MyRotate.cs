d293947a54c5924a2134b4e4be5749bae9bb714b
Realeyz <mark@themarksmen.net>
2016-04-18 17:15:13 -0400

    this code constrains the rotation to within certain angles
    But, I still haven’t figured out how to make decrementing values make
    the object spin in reverse.  This only spins one way.

--- MIDI_Spotlight/Assets/Scripts/MyRotate.cs
+++ MIDI_Spotlight/Assets/Scripts/MyRotate.cs
@@ -7,22 +7,23 @@ public class MyRotate : MonoBehaviour {
 	[SerializeField]
 	private float knobVal; // value of knob
 	[SerializeField]
-	private float rotateSpeed = 1.0f; // In rotations per second
+	// private float rotateSpeed = 1.0f; // In rotations per second
 
+	//Rotation Sensitivity
+	public float RotationSensitivity = 50.0f;
+	public float minAngle = -360.0f;
+	public float maxAngle = 360.0f;
 
-	void Awake()
-	{
+	//Rotation Value
+	float yRotate = 0.0f;
+
+	// Update is called once per frame
+	void Update () {
 		knobVal = MidiMaster.GetKnob(knobNumber);
-		// transform.localScale = new Vector3(1, s, 1);
-		transform.Rotate (transform.up, rotateSpeed * knobVal); //transform.up = around Y axis
+		//Rotate Y view
+		yRotate += 	knobVal * RotationSensitivity * Time.deltaTime;
+		yRotate = Mathf.Clamp (yRotate, minAngle, maxAngle);
+		transform.eulerAngles = new Vector3 (0.0f, yRotate, 0.0f);
 	}
 
-
-	void Update()
-	{
-		// var channels = MidiMaster.GetKnobNumbers();
-		knobVal = MidiMaster.GetKnob(knobNumber);
-		// transform.localScale = new Vector3(1, s, 1);
-		transform.Rotate (transform.up, rotateSpeed * knobVal); //transform.up = around Y axis
-	}
 }
